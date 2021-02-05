72ad7379d433c18b7d779d0b105c53bce6ed539f
amansourian <mansourianarman@gmail.com>
2021-02-05 00:08:36 -0800

    lab completed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,47 +1,62 @@
 // main.js
 
+
+// form element
 partyHornForm = document.getElementById('party-horn-form')
+
+// volume elements
 volumeImage = document.getElementById("volume-image");
 volumeNumber = document.getElementById("volume-number");
 volumeSlider = document.getElementById("volume-slider");
 hornSound = document.getElementById("horn-sound");
+
+// audio selection elements
 soundImage = document.getElementById("sound-image");
 radioAirHorn = document.getElementById("radio-air-horn");
 radioCarHorn = document.getElementById("radio-car-horn");
 radioPartyHorn = document.getElementById("radio-party-horn");
 
+// honk button element
+honkButton = document.getElementById("honk-btn");
+
+
+// volume event listeners
 partyHornForm.addEventListener('submit', function(e) {
     e.preventDefault();
 }, false);
 
-volumeNumber.addEventListener("change", function () {
+volumeNumber.addEventListener("input", function () {
   hornSound.volume = volumeNumber.value / 100; 
   volumeSlider.value = volumeNumber.value;
-
   let n = (volumeNumber.value == 0) ? 0 : ((volumeNumber.value / 33.4) >> 0) + 1;
   volumeImage.src = `./assets/media/icons/volume-level-${n}.svg`;
 });
 
-volumeSlider.addEventListener("mouseup", function() {
+volumeSlider.addEventListener("input", function() {
   hornSound.volume = volumeSlider.value / 100; 
   volumeNumber.value = volumeSlider.value;
-
   let n = (volumeNumber.value == 0) ? 0 : ((volumeNumber.value / 33.4) >> 0) + 1;
   volumeImage.src = `./assets/media/icons/volume-level-${n}.svg`;
 });
 
-radioAirHorn.addEventListener("mouseup", function() {
-  hornSound.src = ".assets/media/audio/air-horn.mp3";
+// audio selection event listeners
+radioAirHorn.addEventListener("click", function() {
+  hornSound.src = "./assets/media/audio/air-horn.mp3";
   soundImage.src = "./assets/media/images/air-horn.svg";
 });
 
-radioCarHorn.addEventListener("mouseup", function() {
-  hornSound.src = ".assets/media/audio/car-horn.mp3";
+radioCarHorn.addEventListener("click", function() {
+  hornSound.src = "./assets/media/audio/car-horn.mp3";
   soundImage.src = "./assets/media/images/car.svg";
 });
 
-radioPartyHorn.addEventListener("mouseup", function() {
-  hornSound.src = ".assets/media/audio/party-horn.mp3";
+radioPartyHorn.addEventListener("click", function() {
+  hornSound.src = "./assets/media/audio/party-horn.mp3";
   soundImage.src = "./assets/media/images/party-horn.svg";
+});
+
+// honk button event listener
+honkButton.addEventListener("click", function() {
+  hornSound.play();
 });
 
