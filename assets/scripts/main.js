82e2853f24f912b04b3e3ce73bc31b89220f2296
amansourian <mansourianarman@gmail.com>
2021-02-04 22:37:36 -0800

    horn type functionality added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,9 @@ volumeNumber = document.getElementById("volume-number");
 volumeSlider = document.getElementById("volume-slider");
 hornSound = document.getElementById("horn-sound");
 soundImage = document.getElementById("sound-image");
+radioAirHorn = document.getElementById("radio-air-horn");
+radioCarHorn = document.getElementById("radio-car-horn");
+radioPartyHorn = document.getElementById("radio-party-horn");
 
 partyHornForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -25,5 +28,20 @@ volumeSlider.addEventListener("mouseup", function() {
 
   let n = (volumeNumber.value == 0) ? 0 : ((volumeNumber.value / 33.4) >> 0) + 1;
   volumeImage.src = `./assets/media/icons/volume-level-${n}.svg`;
+});
+
+radioAirHorn.addEventListener("mouseup", function() {
+  hornSound.src = ".assets/media/audio/air-horn.mp3";
+  soundImage.src = "./assets/media/images/air-horn.svg";
+});
+
+radioCarHorn.addEventListener("mouseup", function() {
+  hornSound.src = ".assets/media/audio/car-horn.mp3";
+  soundImage.src = "./assets/media/images/car.svg";
+});
+
+radioPartyHorn.addEventListener("mouseup", function() {
+  hornSound.src = ".assets/media/audio/party-horn.mp3";
+  soundImage.src = "./assets/media/images/party-horn.svg";
 });
 
